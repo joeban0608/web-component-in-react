075ca80effe28b57c4357bf9d8461317c5e04012
joeban <joeban@twbeex.com>
2024-07-05 13:18:02 +0800

    feat: add WebComponent

---
+++ src/webComponents/XSearch.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+class XSearch extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    const root = ReactDOM.createRoot(this.shadowRoot);
+
+    const SearchComponent = () => {
+      const [queryText, setQueryText] = useState("");
+      // const name = this.getAttribute("name");
+
+      const handleOnChange = (e) => {
+        setQueryText(e.target.value);
+      };
+
+      const url = `https://www.google.com/search?q=${encodeURIComponent(
+        queryText
+      )}`;
+
+      return (
+        <div style={{ display: "flex", gap: "12px" }}>
+          <input
+            onChange={handleOnChange}
+            value={queryText}
+            className="bg-foreground text-primary"
+          />
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            Search
+          </a>
+        </div>
+      );
+    };
+
+    root.render(<SearchComponent />);
+  }
+}
+
+customElements.define("x-search", XSearch);
