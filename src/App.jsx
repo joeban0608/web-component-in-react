37faccdc33afb34c7b08c80c7b25a190aee0881b
joeban <joeban@twbeex.com>
2024-07-08 14:59:19 +0800

    feat: 傳 mock data,  react to web component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import "./App.css";
 import "./webComponents/XSearch";
 function App() {
   const [count, setCount] = useState(0);
-
+  const mockData = [
+    { name: "joe", description: "about joe" },
+    { name: "mac", description: "about mac" },
+  ];
   return (
     <>
       <div>
         <h1>Here is web component x-search</h1>
-        <x-search size="2" imgSrc="logo.png"></x-search>
+        <x-search
+          size="2"
+          imgSrc="logo.png"
+          data-arr={JSON.stringify(mockData)}
+        ></x-search>
         <hr></hr>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- src/webComponents/XSearch.jsx
+++ src/webComponents/XSearch.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
@@ -11,10 +13,13 @@ class XSearch extends HTMLElement {
     const root = ReactDOM.createRoot(this.shadowRoot);
     const size = this.getAttribute("size");
     const imgSrc = this.getAttribute("imgSrc");
+    const dataArrStringify = this.getAttribute("data-arr");
+    const dataArr = JSON.parse(dataArrStringify);
     // root;
     const SearchComponent = () => {
       console.log("size", size);
       console.log("imgSrc", imgSrc);
+      console.log("dataArr", dataArr);
 
       const [queryText, setQueryText] = useState("");
 
@@ -27,22 +32,33 @@ class XSearch extends HTMLElement {
       )}`;
 
       return (
-        <div style={{ display: "flex", gap: "12px" }}>
-          <input
-            onChange={handleOnChange}
-            value={queryText}
-            className="bg-foreground text-primary"
-          />
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            Google Search
-          </a>
-          {imgSrc && (
-            <img
-              src={imgSrc}
-              alt="logo"
-              style={{ width: `${size}rem`, height: `${size}rem` }}
-            ></img>
-          )}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "12px",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ display: "flex", gap: "12px" }}>
+            <input
+              onChange={handleOnChange}
+              value={queryText}
+              className="bg-foreground text-primary"
+            />
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              Google Search
+            </a>
+            {imgSrc && (
+              <img
+                src={imgSrc}
+                alt="logo"
+                style={{ width: `${size}rem`, height: `${size}rem` }}
+              ></img>
+            )}
+          </div>
+
+          <Table data={dataArr} title="Mock Data From React App" />
         </div>
       );
     };
@@ -52,3 +68,77 @@ class XSearch extends HTMLElement {
 }
 
 customElements.define("x-search", XSearch);
+
+const Table = ({ data, title }) => {
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        margin: "0 auto",
+        padding: "16px",
+        color: "black",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "24px",
+          fontWeight: "bold",
+          marginBottom: "16px",
+          color: "white",
+        }}
+      >
+        {title}
+      </h1>
+      <table
+        style={{
+          width: "100%",
+          backgroundColor: "white",
+          borderCollapse: "collapse",
+        }}
+      >
+        <thead>
+          <tr>
+            <th
+              style={{
+                padding: "8px 16px",
+                border: "2px solid gray",
+              }}
+            >
+              Name
+            </th>
+            <th
+              style={{
+                padding: "8px 16px",
+                border: "2px solid gray",
+              }}
+            >
+              Description
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item, index) => (
+            <tr key={index}>
+              <td
+                style={{
+                  padding: "8px 16px",
+                  border: "1px solid gray",
+                }}
+              >
+                {item.name}
+              </td>
+              <td
+                style={{
+                  padding: "8px 16px",
+                  border: "1px solid gray",
+                }}
+              >
+                {item.description}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
